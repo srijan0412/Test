cd83b0a2757c56195c3b4f31aab65cd845b300f0
srijan0412 <srijan.soni01@gmail.com>
2025-01-07 16:05:00 +0530

    commit in test4

---
+++ file2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class File2 {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        System.out.println(Scanner);
+    }
+}
